7212cac21317e90bd5dadf1fd6ef54f824c3cac8
buddhasystem <potekhin@bnl.gov>
2024-07-09 20:46:29 -0400

    More cleanup

--- optical1/opt.cc
+++ optical1/opt.cc
@@ -1,33 +1,3 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-// $Id: example.cc 70284 2013-05-28 17:26:43Z perl $
-//
-/// \file example.cc
-/// \brief Main program of the analysis/ example
-
 #include "DetectorConstruction.hh"
 #include "ActionInitialization.hh"
 
@@ -40,7 +10,7 @@
 #include "G4VisExecutive.hh"
 #include "G4UIExecutive.hh"
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+// ---
 
 int main(int argc,char** argv)
 {
@@ -54,27 +24,13 @@ int main(int argc,char** argv)
     // Construct the default run manager
     auto* runManager = G4RunManagerFactory::CreateRunManager();
     
-    // =============================================
-    // Exercise 3.a
-    // Activate UI-command base scorer.
-    // Add here the lines to activate command line
-    // based scorer.
-    // Remember to add the correct "include" statement
-    // =============================================
-    
-    // Mandatory user initialization classes
-    
-    //====================
-    //The Geometry
     runManager->SetUserInitialization(new DetectorConstruction);
     
-    //====================
     //The Physics
     G4VModularPhysicsList* physicsList = new FTFP_BERT;
     physicsList->RegisterPhysics(new G4StepLimiterPhysics());
     runManager->SetUserInitialization(physicsList);
     
-    //====================
     // User action initialization
     runManager->SetUserInitialization(new ActionInitialization());
     
@@ -104,8 +60,7 @@ int main(int argc,char** argv)
     }
     // Job termination
     // Free the store: user actions, physics_list and detector_description are
-    // owned and deleted by the run manager, so they should not be deleted 
-    // in the main() program !
+    // owned and deleted by the run manager, so they should not be deleted in the main() program !
   
     delete visManager;
     delete runManager;
@@ -113,4 +68,3 @@ int main(int argc,char** argv)
     return 0;
 }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
